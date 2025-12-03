866fc82bf386f664f92c44b8562273f9e1107c77
prethith <prathithchivukula@gmail.com>
2025-12-03 18:52:39 +0530

    fix: removing inputs

--- day2/main.go
+++ day2/main.go
@@ -9,7 +9,8 @@ import (
 )
 
 func main() {
-	fmt.Printf("Part 1: %d", part1())
+	fmt.Printf("Part 1: %d\n", part1())
+	fmt.Printf("Part 2: %d\n", part2())
 }
 
 func part1() int {
@@ -49,4 +50,52 @@ func part1() int {
 		}
 	} 
 	return sum
+}
+
+func part2() int {
+	data, err := os.ReadFile("/home/prathith/Documents/advent_of_code_2025/inputs/day02.txt")
+	if err != nil {
+		log.Fatalf("Failed to open file %s", err)
+	}
+	input := string(data)
+	input = strings.TrimSpace(input)
+	ranges := strings.Split(input, ",")
+
+	sum := 0
+
+	for _, elem := range ranges {
+		elem = strings.TrimSpace(elem)
+		index := strings.Index(elem, "-")
+		start, err := strconv.Atoi(elem[0:index])
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		end, err := strconv.Atoi(elem[(index+1):])
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		for i := start; i <= end; i++ {
+			num_str := strconv.Itoa(i)
+			if repeatedSubstring(num_str) == true {
+				sum += i
+			}
+
+		} 
+	}
+
+	return sum
+}
+
+func repeatedSubstring(s string) bool {
+	doubled := s + s
+	idx := strings.Index(doubled[1:], s)
+
+	if (idx == -1) {
+		return false
+	}
+
+	return idx+1 != len(s)
 }
---
+++ day3/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"log"
+	"strings"
+	"strconv"
+)
+
+func main() {
+	fmt.Printf("Part 1: %d\n", part1())
+	fmt.Printf("Part 2: %d\n", part2())
+}
+
+func part1() int {
+	return 0
+}
+
+func part2() int {
+	return 0
+}
